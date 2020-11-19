96abf63098f62d54dc272bd50a5a4393baef1f68
Ilya Krupin <dev@krupibox.ru>
2020-11-19 16:05:03 +0300

    Add snapshot hoc WithSortToggle

---
+++ src/components/hoc/with-sort-toggle/with-sort-toggle.test.js
@@ -0,0 +1,18 @@
+import withSortToggle from './with-sort-toggle';
+
+describe(`<withSortToggle/>: `, () => {
+
+  const MockComponent = () => <div />;
+
+  const MockWrapper = withSortToggle(MockComponent);
+
+  it(`should render correctly`, () => {
+    const wrapper = renderer
+      .create(
+          <MockWrapper
+            onTypeClick={() => { }}
+          />
+      ).toJSON();
+    expect(wrapper).toMatchSnapshot();
+  });
+});
